312c295003fdb0879b0dd77e2da5f7a082a9ebb3
kubili <kubili2013@gmail.com>
2017-12-31 00:23:53 +0800

    facebook login

--- database/migrations/2017_12_30_160702_add_cloumns_facebook.php
+++ database/migrations/2017_12_30_160702_add_cloumns_facebook.php
@@ -14,9 +14,8 @@ class AddCloumnsFacebook extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('facebook_id', 160)->default('');
-            $table->string('facebook_username', 160)->default('');
-            $table->string('bio', 160)->default('');
+            $table->string('facebook_id', 64)->default('');
+            $table->string('facebook_username',64)->default('');
         });
     }
 
@@ -28,7 +27,8 @@ class AddCloumnsFacebook extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn("facebook_id");
+            $table->dropColumn("facebook_username");
         });
     }
 }
